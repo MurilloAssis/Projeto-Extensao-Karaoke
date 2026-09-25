89808be96183f444e96575e1a46a397ccd1ad4ec
MurilloAssis <murillo98asiss@gmail.com>
2026-09-25 14:23:10 -0300

    Revert "teste"
    This reverts commit fd7a3c9c333597fa5109df0d3f45402e54c6c196.

--- POC/karaoke-envio-player/src/App.jsx
+++ POC/karaoke-envio-player/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 function App() {
   // Estados de Autenticação
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [usuario, setUsuario] = useState("");
   const [senha, setSenha] = useState("");
   
